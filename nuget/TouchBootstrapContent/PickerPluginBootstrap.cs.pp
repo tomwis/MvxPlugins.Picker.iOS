9639282d30c728a658edaee02a915958d9daf661
Tomasz Wiśniewski <tomaszwisniewski00@gmail.com>
2019-12-15 20:00:22 +0100

    Updated nuget

--- nuget/TouchBootstrapContent/PickerPluginBootstrap.cs.pp
+++
@@ -1,7 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace $rootnamespace$.Bootstrap
-{
-    public class PickerPluginBootstrap
-        : MvxLoaderPluginBootstrapAction<MvxPlugins.Picker.iOS.PluginLoader, MvxPlugins.Picker.iOS.Plugin> { }
-}
